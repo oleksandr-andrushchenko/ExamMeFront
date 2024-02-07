b795f4f31b08e167db23e9a32ce96baa7cfd3d8b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-06 22:36:04 -0700

    tailwindcss added; default css removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    const port: number = parseInt(env.PORT);
+  const env = loadEnv(mode, process.cwd(), '');
+  const port: number = parseInt(env.PORT);
 
-    return {
-        plugins: [ react() ],
-        server: {
-            watch: {
-                usePolling: true,
-            },
-            host: '0.0.0.0',
-            port,
-        },
-    }
+  return {
+    plugins: [ react(), tailwindcss() ],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: '0.0.0.0',
+      port,
+    },
+  }
 })
