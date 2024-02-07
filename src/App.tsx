e9bc66ec800048a1f49b2ba41ac373cb8e721e15
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-06 23:33:37 -0700

    container width changed

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export default function App() {
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
@@ -182,12 +182,12 @@ export default function App() {
         </Disclosure>
 
         <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
         </main>
       </div>
     </>
