30cda411883e36439b4d3c2d8acac5873886ecc5
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-07 22:52:52 -0700

    layout implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import './App.css';
-import NavBar from "./components/NavBar.tsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Categories from "./pages/Categories.tsx";
+import Layout from "./components/Layout.tsx";
 
 export default function App() {
   return (
-    <>
-      <div className="min-h-full">
-        <Router>
-          <NavBar width={4}/>
-          <Routes>
-            <Route index element={<Home/>}/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/categories" element={<Categories/>}/>
-          </Routes>
-        </Router>
-
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
-        </main>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/categories" element={<Categories/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
