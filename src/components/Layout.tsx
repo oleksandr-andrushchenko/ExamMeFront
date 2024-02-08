99d4b539bde58303e878ddfb2dd49829b3fe7cc7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 16:11:32 -0700

    header removed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,13 @@
 import NavBar from "./NavBar";
-import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div className="min-h-full">
-      <NavBar width={4}/>
-      <Header width={4}/>
+      <NavBar/>
       <main>
         <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
-          <Outlet />
+          <Outlet/>
         </div>
       </main>
     </div>
