39f963911ba7cc5236d572b8baf317367babf2ff
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 16:12:36 -0700

    material tailwind implemented; refactored

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,157 +1,107 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, LightBulbIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { LightBulbIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import classNames from "../utils/classNames";
+import React from "react";
+import { Navbar, Collapse, Typography, Button, IconButton } from "@material-tailwind/react";
 
 const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Categories', href: '/categories' },
-]
+];
 
-export default function NavBar({ width }) {
+export default function NavBar() {
+  const [ openNav, setOpenNav ] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      {navigation.map((item) => {
+        const resolvedPath = useResolvedPath(item.href);
+        const current = useMatch({ path: resolvedPath.pathname, end: true });
+
+        return <Typography
+          as="li"
+          key={item.href}
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-normal"
+        >
+          <Link
+            key={item.name}
+            to={item.href}
+            className={classNames(
+              current ? 'underline' : '',
+              'flex items-center'
+            )}
+            aria-current={current ? 'page' : undefined}
+          >
+            {item.name}
+          </Link>
+        </Typography>
+      })}
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Button
+          variant="filled"
+          color="pink"
+          size="md"
+        >
+          <span>Log In</span>
+        </Button>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Button
+          variant="filled"
+          size="sm"
+        >
+          <span>Sign in</span>
+        </Button>
+      </Typography>
+    </ul>
+  );
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className={classNames('mx-auto px-2 sm:px-6 lg:px-8', `max-w-${width}xl`)}>
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button
-                  className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5"/>
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <LightBulbIcon className={classNames('block h-10 w-10', 'text-white')} aria-hidden="true"
-                                 color="white" alt="ExamMe"/>
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => {
-                      const resolvedPath = useResolvedPath(item.href);
-                      const current = useMatch({ path: resolvedPath.pathname, end: true });
-
-                      return <Link
-                        key={item.name}
-                        to={item.href}
-                        className={classNames(
-                          current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </Link>
-                    })}
-                  </div>
-                </div>
-              </div>
-              <div
-                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5"/>
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true"/>
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button
-                      className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5"/>
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              asd
-              {navigation.map((item) => {
-                const resolvedPath = useResolvedPath(item.href);
-                const current = useMatch({ path: resolvedPath.pathname, end: true });
-
-                return <Disclosure.Button
-                  key={item.name}
-                  as="Link"
-                  to={item.href}
-                  className={classNames(
-                    current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              })}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  )
+    <Navbar className="h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4" fullWidth={true}>
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <Link
+          to="/"
+          className="mr-4 cursor-pointer py-1.5 text-xs"
+        >
+          <LightBulbIcon alt="ExamMe"/> Exam Me
+        </Link>
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav
+              ? <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
+              : <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>}
+          </IconButton>
+        </div>
+      </div>
+      <Collapse open={openNav}>
+        {navList}
+      </Collapse>
+    </Navbar>
+  );
 }
