5bbfc635aebbf841b4a4b5ed82adc2a4f852893d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 16:10:27 -0700

    material tailwind added; screens customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -12,7 +13,16 @@ export default {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      sm: "540px",
+      md: "720px",
+      lg: "840px",
+      "lg-max": {max: "840px"},
+      xl: "840px",
+      "2xl": "840px",
+      "2xl-max": {max: "840px"},
+    }
   },
   plugins: [],
-}
+})
 
