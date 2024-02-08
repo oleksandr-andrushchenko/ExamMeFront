fc2b5857f798d6a767ed6a6606c27fbfda5fe6f2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-07 22:57:07 -0700

    router provider implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'
+import './index.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import Home from "./pages/Home.tsx";
+import Categories from "./pages/Categories.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout/>}>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/categories" element={<Categories/>}/>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
