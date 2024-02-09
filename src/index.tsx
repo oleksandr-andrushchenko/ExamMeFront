79a3dadbccd92745ae9f363ba8df170756fbb0ba
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 18:23:22 -0700

    data provider for categories added

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,13 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import ThemeProvider from "./components/ThemeProvider";
+import categoriesLoader from "./loaders/categoriesLoader";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout/>}>
       <Route path="/" element={<Home/>}/>
-      <Route path="/categories" element={<Categories/>}/>
+      <Route path="/categories" element={<Categories/>} loader={categoriesLoader}/>
     </Route>
   )
 );
