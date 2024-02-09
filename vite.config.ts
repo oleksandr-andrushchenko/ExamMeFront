6308a5784fc41558f6b0dd8c0c6ad2dfae01a227
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 17:23:05 -0700

    outdated stuff removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const port: number = parseInt(env.PORT);
 
   return {
-    plugins: [ react(), tailwindcss() ],
+    plugins: [ react() ],
     server: {
       watch: {
         usePolling: true,
