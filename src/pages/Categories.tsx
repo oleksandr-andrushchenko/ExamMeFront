8e013b671b58015a7946040ce166afb1c0998bfe
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-08 18:21:36 -0700

    simple categories rendering added

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,7 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { List, ListItem } from '@material-tailwind/react';
+import { Link } from "react-router-dom";
+
 export default function Categories() {
+  const categories = useLoaderData();
+
   return (
     <>
-      Categories
+      <h1>Categories</h1>
+
+      <List>
+        {categories.map(category => {
+          return <ListItem key={category.id}>
+            <Link key={category.id} to={`/categories/${category.id}`}>
+              {category.name}
+            </Link>
+          </ListItem>;
+        })}
+      </List>
     </>
   );
 }
