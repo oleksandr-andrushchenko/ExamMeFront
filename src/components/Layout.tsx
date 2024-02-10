1be2341f3234c552b2961774ed4ce2c7884e1167
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-09 21:30:25 -0700

    spinner on data loading

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Spinner from "./Spinner";
 
 export default function Layout() {
+  const navigation = useNavigation();
+
   return (
     <div className="min-h-full">
       <NavBar/>
       <main>
         <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
-          <Outlet/>
+          {navigation.state === "loading" ? <Spinner/> : <Outlet/>}
         </div>
       </main>
     </div>
