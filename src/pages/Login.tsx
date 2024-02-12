67679bea6da8213cd07bffbe250b85bcf46438aa
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-11 23:16:52 -0700

    login form ui improvements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,63 +1,46 @@
+import { Form } from 'react-router-dom';
+import { Button, Card, Input, Typography } from "@material-tailwind/react";
+import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/solid";
+
 export default function Login() {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Log in
-        </h2>
-      </div>
-
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Password
-              </label>
-              <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Log in
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
+    <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        <ArrowRightEndOnRectangleIcon className="inline-block h-6 w-6"/> Login
+      </Typography>
+      <Typography variant="small" color="gray" className="mt-1 font-normal">
+        Enter your details to login
+      </Typography>
+      <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" method="post">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+            name="email"
+            size="lg"
+            label="Email Address"
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+            name="password"
+            type="password"
+            size="lg"
+            label="Password"
+          />
+        </div>
+        <Button className="block rounded capitalize mt-4" type="submit">
+          Login
+        </Button>
+        <Typography variant="small" color="gray" className="mt-4 font-normal">
+          Don't have an account?{" "}
+          <a href="/register" className="font-medium text-gray-900">
+            Register
+          </a>
+        </Typography>
+      </Form>
+    </Card>
+  );
 }
