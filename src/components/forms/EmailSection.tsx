b509df8aedb5f7b3a89c9bb868bdc6258e150444
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 16:37:28 -0700

    email form section component added

---
+++ src/components/forms/EmailSection.tsx
@@ -0,0 +1,50 @@
+import { Input, Typography } from "@material-tailwind/react";
+import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+
+export type EmailSectionProps = {
+  setReadyState: (ready: boolean) => void,
+}
+
+export default function EmailSection({ setReadyState }: EmailSectionProps) {
+
+  const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const [ email, setEmail ] = useState('');
+  const [ emailValid, setEmailValid ] = useState(true);
+  const [ emailFocused, setEmailFocused ] = useState(false);
+
+  useEffect(() => setEmailValid(EMAIL_REGEX.test(email)), [ email ]);
+  useEffect(() => setReadyState(email !== '' && emailValid), [ email, emailValid ]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Typography variant="h6" color={!emailFocused && email !== '' && !emailValid ? "red" : "blue-gray"}>
+        Your Email
+      </Typography>
+      <Input
+        name="email"
+        type="email"
+        size="lg"
+        label="Email Address"
+        onChange={(e) => setEmail(e.target.value)}
+        onFocus={() => setEmailFocused(true)}
+        onBlur={() => setEmailFocused(false)}
+        value={email}
+        aria-invalid={email != '' && emailValid ? "false" : "true"}
+        error={!emailFocused && email !== '' && !emailValid}
+        required
+      />
+      <Typography
+        variant="small"
+        color={!emailFocused && email !== '' && !emailValid ? "red" : "gray"}
+        className="flex items-center gap-1 font-normal"
+      >
+        <ExclamationCircleIcon className="w-1/12"/>
+        <span className="w-11/12">
+          Should be valid email address
+        </span>
+      </Typography>
+    </div>
+  );
+}
