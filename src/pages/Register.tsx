6ae69e9cddaf531357b0a054265ca3ed7856202e
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 16:38:53 -0700

    email & password form section components implemented

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,10 @@
 import { Form, Link } from 'react-router-dom';
 import { Card, Input, Checkbox, Button, Typography } from "@material-tailwind/react";
 import apiClient from "../api/apiClient";
-import { PlusIcon, ExclamationCircleIcon } from "@heroicons/react/24/solid";
-import { useEffect, useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import EmailSection from "../components/forms/EmailSection.tsx";
+import PasswordSection from "../components/forms/PasswordSection.tsx";
 
 export async function registerAction({ request }) {
   const formData = await request.formData();
@@ -18,22 +20,9 @@ export async function registerAction({ request }) {
 
 export default function Register() {
 
-  const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,24}$/;
-  const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  const [ email, setEmail ] = useState('');
-  const [ emailValid, setEmailValid ] = useState(true);
-  const [ emailFocused, setEmailFocused ] = useState(false);
-  const [ password, setPassword ] = useState('');
-  const [ passwordValid, setPasswordValid ] = useState(true);
-  const [ passwordFocused, setPasswordFocused ] = useState(false);
-  const [ confirmPassword, setConfirmPassword ] = useState('');
-  const [ passwordsMatches, setPasswordsMatches ] = useState(true);
+  const [ emailReady, setEmailReady ] = useState(true);
+  const [ passwordReady, setPasswordReady ] = useState(true);
   const [ terms, setTerms ] = useState(false);
-
-  useEffect(() => setEmailValid(EMAIL_REGEX.test(email)), [ email ]);
-  useEffect(() => setPasswordValid(PASSWORD_REGEX.test(password)), [ password ]);
-  useEffect(() => setPasswordsMatches(password === confirmPassword), [ password, confirmPassword ]);
 
   return (
     <Card color="transparent" shadow={false}>
@@ -45,93 +34,8 @@ export default function Register() {
       </Typography>
       <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-6" method="post">
 
-        <div className="flex flex-col gap-2">
-          <Typography variant="h6" color={!emailFocused && email !== '' && !emailValid ? "red" : "blue-gray"}>
-            Your Email
-          </Typography>
-          <Input
-            name="email"
-            type="email"
-            size="lg"
-            label="Email Address"
-            onChange={(e) => setEmail(e.target.value)}
-            onFocus={() => setEmailFocused(true)}
-            onBlur={() => setEmailFocused(false)}
-            value={email}
-            aria-invalid={email != '' && emailValid ? "false" : "true"}
-            error={!emailFocused && email !== '' && !emailValid}
-            required
-          />
-          <Typography
-            variant="small"
-            color={!emailFocused && email !== '' && !emailValid ? "red" : "gray"}
-            className="flex items-center gap-1 font-normal"
-          >
-            <ExclamationCircleIcon className="w-1/12"/>
-            <span className="w-11/12">
-                Should be valid email address
-              </span>
-          </Typography>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <Typography variant="h6"
-                      color={!passwordFocused && password !== '' && !passwordValid ? "red" : "blue-gray"}>
-            Password
-          </Typography>
-          <Input
-            name="password"
-            type="password"
-            size="lg"
-            label="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            onFocus={() => setPasswordFocused(true)}
-            onBlur={() => setPasswordFocused(false)}
-            value={password}
-            aria-invalid={password != '' && passwordValid ? "false" : "true"}
-            error={!passwordFocused && password !== '' && !passwordValid}
-            required
-          />
-          <Typography
-            variant="small"
-            color={!passwordFocused && password !== '' && !passwordValid ? "red" : "gray"}
-            className="flex items-center gap-1 font-normal"
-          >
-            <ExclamationCircleIcon className="w-1/12"/>
-            <span className="w-11/12">
-                Should be from 8 to 24 characters long, uppercase and lowercase letters, a number and a special
-                character
-              </span>
-          </Typography>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <Typography variant="h6" color={!passwordFocused && !passwordsMatches ? "red" : "blue-gray"}>
-            Confirm Password
-          </Typography>
-          <Input
-            name="confirm-password"
-            type="password"
-            size="lg"
-            label="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-            aria-invalid={passwordsMatches ? "false" : "true"}
-            aria-describedby="confirmnote"
-            error={!passwordFocused && !passwordsMatches}
-            required
-          />
-          <Typography
-            variant="small"
-            color={!passwordFocused && !passwordsMatches ? "red" : "gray"}
-            className="flex items-center gap-1 font-normal"
-          >
-            <ExclamationCircleIcon className="w-1/12"/>
-            <span className="w-11/12">
-                Should match the password
-              </span>
-          </Typography>
-        </div>
+        <EmailSection setReadyState={setEmailReady}/>
+        <PasswordSection setReadyState={setPasswordReady} confirm/>
 
         <Checkbox
           label={
@@ -149,8 +53,7 @@ export default function Register() {
         />
 
         <div>
-          <Button className="block rounded capitalize" type="submit"
-                  disabled={email === '' || !emailValid || password === '' || !passwordValid || !passwordsMatches || !terms}>
+          <Button className="block rounded capitalize" type="submit" disabled={!emailReady || !passwordReady || !terms}>
             Register
           </Button>
 
