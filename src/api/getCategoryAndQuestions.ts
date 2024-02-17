1ced9ec364c274989f7c079d4e0bbda91bb82c1e
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 22:11:13 -0700

    get category and questions api service added

---
+++ src/api/getCategoryAndQuestions.ts
@@ -0,0 +1,25 @@
+import apiClient from "../api/apiClient";
+import { AxiosResponse } from "axios";
+import Question from "../schema/Question";
+import Category from "../schema/Category";
+
+interface getCategoryAndQuestionsParams {
+  params: { categoryId: string }
+}
+
+interface getCategoryAndQuestionsResult {
+  category: Category,
+  questions: Question[],
+}
+
+export const getCategoryAndQuestions = async ({ params }: getCategoryAndQuestionsParams): Promise<getCategoryAndQuestionsResult> => {
+  const response: AxiosResponse[] = await Promise.all([
+    apiClient.get(`/categories/${params.categoryId}`),
+    apiClient.get(`/categories/${params.categoryId}/questions`),
+  ]);
+
+  return {
+    category: response[0].data,
+    questions: response[1].data,
+  };
+};
