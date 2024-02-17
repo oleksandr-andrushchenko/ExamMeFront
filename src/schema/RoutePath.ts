d17c8abc2a5c67f9cd6b10140ef13f6f83e5c50f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-17 01:47:01 -0700

    route path type added

---
+++ src/schema/RoutePath.ts
@@ -0,0 +1,12 @@
+enum RoutePath {
+  HOME = '/',
+  CATEGORIES = '/categories',
+  CATEGORY = '/categories/:categoryId',
+  ADD_CATEGORY = '/categories/add',
+  LOGIN = '/login',
+  REGISTER = '/register',
+  TERMS_AND_CONDITIONS = '/terms-and-conditions',
+  UNAUTHORIZED = '/unauthorized',
+}
+
+export default RoutePath;
