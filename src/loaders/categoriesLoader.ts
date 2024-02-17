03c9fd513c25487262a0459d50fde7ac9e13a62c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 22:12:29 -0700

    old loader removed

--- src/loaders/categoriesLoader.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from "../api/apiClient";
-import { AxiosResponse } from "axios";
-
-export default async function categoriesLoader() {
-  const response: AxiosResponse = await apiClient.get('/categories');
-
-  return response.data;
-}
