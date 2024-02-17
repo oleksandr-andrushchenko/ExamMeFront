d7e1297322374bb7abe3370eaadd01d4d820f6d4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 21:41:27 -0700

    form on submit handler added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate, useLocation } from 'react-router-dom';
 import { Button, Card, Typography } from "@material-tailwind/react";
 import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/solid";
 import apiClient from "../api/apiClient";
 import { useState } from "react";
 import EmailSection from "../components/forms/EmailSection.tsx";
 import PasswordSection from "../components/forms/PasswordSection.tsx";
+import useAuth from "../hooks/useAuth";
 
 export async function loginAction({ request }) {
   const formData = await request.formData();
@@ -22,6 +23,38 @@ export default function Login() {
 
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const loginRes = await apiClient.post('/auth', { email, password });
+      const token = loginRes?.data?.token;
+      const config = {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      };
+      const meRes = await apiClient.get('/me', config);
+
+      setAuth({
+        email,
+        permissions: meRes?.data?.permissions,
+        token,
+        expires: loginRes?.data?.expires,
+      });
+      setEmail('');
+      setPassword('');
+      navigate(from, { replace: true });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card color="transparent" shadow={false}>
@@ -31,7 +64,8 @@ export default function Login() {
       <Typography variant="small" color="gray" className="mt-1 font-normal">
         Enter your details to login
       </Typography>
-      <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-6" method="post">
+      <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-6" onSubmit={handleSubmit}
+            method="post">
 
         <EmailSection setValue={setEmail}/>
         <PasswordSection setValue={setPassword}/>
