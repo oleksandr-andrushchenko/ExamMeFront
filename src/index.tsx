725e9699afb1c9cc666d80690b5bcdd8005fc3f4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 22:12:15 -0700

    new api services implemented

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import ThemeProvider from "./components/ThemeProvider";
 import Category from "./pages/Category";
-import categoriesLoader from "./loaders/categoriesLoader";
-import categoryLoader from "./loaders/categoryLoader";
+import { getCategories } from "./api/getCategories";
+import { getCategoryAndQuestions } from "./api/getCategoryAndQuestions";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
@@ -19,8 +19,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout/>}>
       <Route path="/" element={<Home/>}/>
-      <Route path="/categories" element={<Categories/>} loader={categoriesLoader}/>
-      <Route path="/categories/:categoryId" element={<Category/>} loader={categoryLoader}/>
+      <Route path="/categories" element={<Categories/>} loader={getCategories}/>
+      <Route path="/categories/:categoryId" element={<Category/>} loader={getCategoryAndQuestions}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/terms-and-conditions" element={<TermsAndConditions/>}/>
