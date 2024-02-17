177534f41799d1e55d9d579656bffbb2217c9660
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 22:11:56 -0700

    response type added

--- src/api/getMe.ts
+++ src/api/getMe.ts
@@ -1,4 +1,9 @@
 import apiClient from "../api/apiClient";
 import Me from "../schema/Me";
+import { AxiosResponse } from "axios";
 
-export const getMe = async (): Promise<Me> => (await apiClient.get('/me')).data;
+export const getMe = async (): Promise<Me> => {
+  const response: AxiosResponse = await apiClient.get('/me');
+
+  return response.data;
+};
