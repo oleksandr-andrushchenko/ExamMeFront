4863a898089f35d9b34f047f0219c3b349943241
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-17 02:17:29 -0700

    loader data type implemented; category href generated from route path

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import { List, ListItem } from '@material-tailwind/react';
 import { Link } from "react-router-dom";
+import Category from "../schema/Category";
+import RoutePath from "../schema/RoutePath";
 
 export default function Categories() {
-  const categories = useLoaderData();
+  const categories = useLoaderData() as Category[];
 
   return (
     <>
@@ -12,7 +14,9 @@ export default function Categories() {
       <List>
         {categories.map(category => {
           return <ListItem key={category.id}>
-            <Link key={category.id} to={`/categories/${category.id}`}>
+            <Link
+              key={category.id}
+              to={RoutePath.CATEGORY.replace(':categoryId', category.id)}>
               {category.name}
             </Link>
           </ListItem>;
