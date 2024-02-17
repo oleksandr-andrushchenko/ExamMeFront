75c0d45933692c2c6efbe5e8edd77ac3664129b6
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-16 21:45:44 -0700

    form on submit handler added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,11 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate, useLocation } from 'react-router-dom';
 import { Card, Checkbox, Button, Typography } from "@material-tailwind/react";
 import apiClient from "../api/apiClient";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import EmailSection from "../components/forms/EmailSection.tsx";
 import PasswordSection from "../components/forms/PasswordSection.tsx";
+import useAuth from "../hooks/useAuth";
 
 export async function registerAction({ request }) {
   const formData = await request.formData();
@@ -23,6 +24,32 @@ export default function Register() {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ terms, setTerms ] = useState(false);
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const registerRes = await apiClient.post('/me', { email, password });
+      const loginRes = await apiClient.post('/auth', { email, password });
+
+      setAuth({
+        email,
+        permissions: registerRes?.data?.permissions,
+        token: loginRes?.data?.token,
+        expires: loginRes?.data?.expires,
+      });
+      setEmail('');
+      setPassword('');
+      navigate(from, { replace: true });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card color="transparent" shadow={false}>
@@ -32,7 +59,8 @@ export default function Register() {
       <Typography variant="small" color="gray" className="mt-1 font-normal">
         Enter your details to register
       </Typography>
-      <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-6" method="post">
+      <Form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col gap-6" onSubmit={handleSubmit}
+            method="post">
 
         <EmailSection setValue={setEmail}/>
         <PasswordSection setValue={setPassword} confirm/>
