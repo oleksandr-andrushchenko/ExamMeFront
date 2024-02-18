a714f5b33a408bd894bdeed7169b19659bca3296
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-17 21:07:39 -0700

    go back button updated

--- src/pages/Unauthorized.tsx
+++ src/pages/Unauthorized.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
+import React from "react";
+import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid";
 
 export default function Unauthorized() {
   const navigate = useNavigate();
@@ -10,9 +13,12 @@ export default function Unauthorized() {
       <h1>Unauthorized</h1>
       <br/>
       <p>You do not have access to the requested page.</p>
-      <div className="flexGrow">
-        <button onClick={goBack}>Go Back</button>
-      </div>
+      <Button
+        size="sm"
+        className="rounded capitalize"
+        onClick={goBack}>
+        <ArrowUturnLeftIcon className="inline-block h-4 w-4"/> Go Back
+      </Button>
     </>
   )
 }
