659ab6602027c0922cf2a88a834e913f81344b7c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-18 02:22:38 -0700

    title text & styles updated; subtitle added; refactoring

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 import Route from "../enum/Route";
+import { Button, Typography } from "@material-tailwind/react";
+import { HomeIcon, PuzzlePieceIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
-const NotFound = () => {
+export default () => {
   return (
     <>
-      <h1>Oops!</h1>
-      <p>Page Not Found</p>
-      <div className="flexGrow">
-        <Link to={Route.HOME}>Visit Our Homepage</Link>
-      </div>
+      <Typography variant="h1" color="blue-gray" className="flex items-baseline">
+        <PuzzlePieceIcon className="inline-block h-8 w-8 mr-1"/> Not Found
+      </Typography>
+      <Typography variant="small" color="gray" className="mt-1 font-normal">
+        Page not found
+      </Typography>
+      <Link to={Route.HOME}>
+        <Button
+          size="sm"
+          className="rounded capitalize font-normal mt-3">
+          <HomeIcon className="inline-block h-4 w-4"/> Go Home
+        </Button>
+      </Link>
     </>
   )
 }
-
-export default NotFound
