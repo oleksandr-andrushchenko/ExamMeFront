57afd91478c8f59146bc9732bea8305f3b8aaa20
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-18 02:20:21 -0700

    title styles updated; subtitle added; refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { Typography } from "@material-tailwind/react";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import React from "react";
+
+export default () => {
   return (
     <>
-      <h1>Home</h1>
+      <Typography variant="h1" color="blue-gray" className="flex items-baseline">
+        <HomeIcon className="inline-block h-8 w-8 mr-1"/> Home
+      </Typography>
+      <Typography variant="small" color="gray" className="mt-1 font-normal">
+        Latest activities
+      </Typography>
     </>
   );
 }
