169c7ac81c776edf4d396fbfaf6016141e410fe6
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-18 02:20:02 -0700

    title styles updated; subtitle added; add question link added; refactoring

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,14 +1,23 @@
 import { Link, useLoaderData } from "react-router-dom";
-import { List, ListItem } from "@material-tailwind/react";
+import { Button, List, ListItem, Typography } from "@material-tailwind/react";
 import { CategoryAndQuestions } from "../loaders/getCategoryAndQuestions";
 import Route from "../enum/Route";
+import { CubeIcon, SquaresPlusIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import useAuth from "../hooks/useAuth";
 
-export default function Category() {
+export default () => {
   const [ category, questions ]: CategoryAndQuestions = useLoaderData();
+  const { auth } = useAuth();
 
   return (
     <>
-      <h1 className="capitalize">{category.name}</h1>
+      <Typography variant="h1" color="blue-gray" className="flex items-baseline">
+        <CubeIcon className="inline-block h-8 w-8 mr-1"/> <span className="capitalize">{category.name}</span>
+      </Typography>
+      <Typography variant="small" color="gray" className="mt-1 font-normal">
+        Available questions
+      </Typography>
 
       <List>
         {questions.map(question => {
@@ -21,6 +30,15 @@ export default function Category() {
           </ListItem>;
         })}
       </List>
+
+      {auth && <Link
+        to={Route.ADD_QUESTION.replace(':categoryId', category.id)}>
+        <Button
+          size="sm"
+          className="rounded capitalize font-normal mt-3">
+          <SquaresPlusIcon className="inline-block h-4 w-4"/> Add Question
+        </Button>
+      </Link>}
     </>
   );
 }
