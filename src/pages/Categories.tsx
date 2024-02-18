4413fca87d1a6930486514e8506399cfcb6b31a8
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-17 21:02:43 -0700

    add category link added

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,15 +1,28 @@
 import { useLoaderData } from "react-router-dom";
-import { List, ListItem } from '@material-tailwind/react';
+import { Button, List, ListItem } from '@material-tailwind/react';
 import { Link } from "react-router-dom";
 import Category from "../schema/Category";
 import RoutePath from "../schema/RoutePath";
+import useAuth from "../hooks/useAuth";
+import { SquaresPlusIcon } from "@heroicons/react/16/solid";
+import React from "react";
 
 export default function Categories() {
   const categories = useLoaderData() as Category[];
+  const { auth } = useAuth();
 
   return (
     <>
       <h1>Categories</h1>
+
+      {auth && <Link
+        to={RoutePath.ADD_CATEGORY}>
+        <Button
+          size="sm"
+          className="rounded capitalize">
+          <SquaresPlusIcon className="inline-block h-4 w-4"/> Add Category
+        </Button>
+      </Link>}
 
       <List>
         {categories.map(category => {
