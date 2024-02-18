9d61807efb7808306b7c3e76952a45bdf63d63db
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-18 16:50:51 -0700

    get permission hierarchy api service added

---
+++ src/api/getPermissionHierarchy.ts
@@ -0,0 +1,4 @@
+import client from "./client";
+import PermissionHierarchy from "../types/PermissionHierarchy";
+
+export default async (): Promise<PermissionHierarchy> => (await client.get('/permissions/hierarchy')).data;
