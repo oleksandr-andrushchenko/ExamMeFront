c2bd52db87d4361561bcc3bcc793eb560f27ca0d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-18 02:23:35 -0700

    title styles updated; subtitle added; refactoring

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,7 +1,16 @@
-export default function TermsAndConditions() {
+import { Typography } from "@material-tailwind/react";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
+
+export default () => {
   return (
     <>
-      <h1>Terms and conditions</h1>
+      <Typography variant="h1" color="blue-gray" className="flex items-baseline">
+        <InformationCircleIcon className="inline-block h-8 w-8 mr-1"/>
+        <span className="capitalize">Terms and conditions</span>
+      </Typography>
+      <Typography variant="small" color="gray" className="mt-1 font-normal">
+        Our rules and policies
+      </Typography>
     </>
   );
 }
