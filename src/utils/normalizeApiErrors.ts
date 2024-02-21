725cc3588711b2f8379dea0f1a02d5335703ab29
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-21 01:24:40 -0700

    api errors normalizer added

---
+++ src/utils/normalizeApiErrors.ts
@@ -0,0 +1,28 @@
+export default (err): object => {
+  const data = err.response.data;
+  console.log(data);
+
+  if (data?.errors.length > 0) {
+    return data.errors;
+  }
+
+  if (data?.message) {
+    const staticValidationPart = 'Validation errors ';
+    if (data.message.startsWith(staticValidationPart)) {
+      const validationErrors = JSON.parse(data.message.substring(staticValidationPart.length));
+
+      // Validation error [{\"value\":\"qwe\",\"property\":\"title\",\"children\":[],\"constraints\":{\"isLength\":\"title must be longer than or equal to 50 characters\"}}]
+      const errors = {};
+
+      for (const validationError of validationErrors) {
+        errors[validationError['property']] = Object.values(validationError['constraints'])
+          .map((error) => error[0].toUpperCase() + error.slice(1))
+          .join("\r\n");
+      }
+
+      return errors;
+    }
+  }
+
+  return { unknown: data.name };
+}
