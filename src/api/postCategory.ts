f549ddb4e5c8db47a5f86299c6627d47219b7bb2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-21 02:06:26 -0700

    non-func refactoring

--- src/api/postCategory.ts
+++ src/api/postCategory.ts
@@ -1,5 +1,5 @@
 import client from "./client";
 import Category from "../schema/Category";
-import CategoryTransfer from "../schema/CategoryTransfer.ts";
+import CategoryTransfer from "../schema/CategoryTransfer";
 
 export default async (transfer: CategoryTransfer): Promise<Category> => (await client.post('/categories', transfer)).data;
