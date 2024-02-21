c7dc58526f9f3f32b333d8eea2267af042e7dc05
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-20 23:52:48 -0700

    common input state & default input state added

---
+++ src/types/InputState.ts
@@ -0,0 +1,13 @@
+export default interface InputState {
+  value: any,
+  focused: boolean,
+  error: string,
+  displayError: boolean,
+}
+
+export const defaultInputState: InputState = {
+  value: '',
+  focused: false,
+  error: '',
+  displayError: false,
+}
