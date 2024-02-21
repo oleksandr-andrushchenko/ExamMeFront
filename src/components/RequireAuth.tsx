e7e77f1d52adc0b56910ff0ba265ed2796c9dfc8
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-21 01:38:05 -0700

    show login page inplace; non-func refactoring

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,27 +1,18 @@
-import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Permission from "../enum/Permission";
-import Route from "../enum/Route";
 import Spinner from "./Spinner";
 import Unauthorized from "../pages/Unauthorized";
+import Login from "../pages/Login";
 
 interface RequireAuthParams {
   permission: Permission,
 }
 
-export default function RequireAuth({ permission }: RequireAuthParams) {
+export default ({ permission }: RequireAuthParams) => {
   const { auth, me, checkAuth } = useAuth();
-  const location = useLocation();
 
   return (
-    auth && me === undefined
-      ? <Spinner/>
-      : (
-        checkAuth(permission)
-          ? <Outlet/>
-          : me
-            ? <Unauthorized/>
-            : <Navigate to={Route.LOGIN} state={{ from: location }} replace/>
-      )
+    auth && me === undefined ? <Spinner/> : (checkAuth(permission) ? <Outlet/> : (me ? <Unauthorized/> : <Login/>))
   );
 }
