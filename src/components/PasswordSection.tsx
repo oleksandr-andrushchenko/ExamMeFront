22d30cbf5eb1604c8c732dd4ba8b0d0d54386d8a
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-21 22:27:55 -0700

    refactoring: input state implemented, formatting; custom error support added

--- src/components/PasswordSection.tsx
+++ src/components/PasswordSection.tsx
@@ -1,46 +1,66 @@
-import { Input, Typography } from "@material-tailwind/react";
+import { Input, ThemeProvider, Typography } from "@material-tailwind/react";
 import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
-import { MutableRefObject, useEffect, useRef, useState } from "react";
-import classNames from "../utils/classNames";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import InputState, { defaultInputState } from "../types/InputState";
 
 interface PasswordSectionProps {
   setValue: (password?: string) => void,
+  error: string,
   confirm?: boolean,
 }
 
-export default function PasswordSection({ setValue, confirm = false }: PasswordSectionProps) {
+export default function PasswordSection({ setValue, error, confirm = false }: PasswordSectionProps) {
 
-  const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,24}$/;
+  const [ password, setPassword ] = useState<InputState>({ ...defaultInputState });
+  const getPasswordError = (value: string | undefined = undefined) => {
+    value = value === undefined ? password.value : value;
 
-  const [ password, setPassword ] = useState('');
-  const [ valid, setValid ] = useState(true);
-  const [ error, setError ] = useState(false);
-  const [ focused, setFocused ] = useState(false);
-  const [ displayError, setDisplayError ] = useState(false);
-
-  const [ confirmPassword, setConfirmPassword ] = useState('');
-  const [ matches, setMatches ] = useState(true);
-  const [ confirmError, setConfirmError ] = useState(false);
-  const [ confirmFocused, setConfirmFocused ] = useState(false);
-  const [ displayConfirmError, setDisplayConfirmError ] = useState(false);
-
-  useEffect(() => setValid(PASSWORD_REGEX.test(password)), [ password ]);
-  useEffect(() => setMatches(password === confirmPassword), [ password, confirmPassword ]);
-  useEffect(() => {
-    setValue(password !== '' && valid && (!confirm || matches) ? password : undefined);
-    setError(password !== '' && !valid);
-    setConfirmError(confirmPassword !== '' && !matches);
-  }, [ password, valid, confirmPassword, matches ]);
-  useEffect(() => {
-    if (error && !focused) {
-      setDisplayError(true);
+    if (!value) {
+      return 'Should not be empty';
     }
-  }, [ focused ]);
-  useEffect(() => {
-    if (confirmError && !confirmFocused) {
-      setDisplayConfirmError(true);
+
+    if (value && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,24}$/.test(value)) {
+      return 'Should be from 8 to 24 characters long, uppercase and lowercase letters, a number and a special character';
     }
-  }, [ confirmFocused ]);
+
+    return '';
+  }
+  const setPasswordValue = (value: string) => {
+    const error = getPasswordError(value);
+    setValue(error ? undefined : value);
+    setPassword({ ...password, ...{ value, error } });
+  }
+  const setPasswordFocused = (focused: boolean) => {
+    const error = focused ? password.error : getPasswordError();
+    const displayError = error && !focused ? true : password.displayError;
+    setPassword({ ...password, ...{ focused, error, displayError } });
+  }
+  const setPasswordError = (error: string) => {
+    const displayError = !!error;
+    setPassword({ ...password, ...{ error, displayError } });
+  }
+  const [ confirmPassword, setConfirmPassword ] = useState<InputState>({ ...defaultInputState });
+  const getConfirmPasswordError = (value: string | undefined = undefined) => {
+    value = value === undefined ? confirmPassword.value : value;
+
+    if (confirm && value !== password.value) {
+      return 'Should match the password';
+    }
+
+    return '';
+  }
+  const setConfirmPasswordValue = (value: string) => {
+    const error = getConfirmPasswordError(value);
+    setConfirmPassword({ ...confirmPassword, ...{ value, error } });
+  }
+  const setConfirmPasswordFocused = (focused: boolean) => {
+    const error = focused ? confirmPassword.error : getConfirmPasswordError();
+    const displayError = error && !focused ? true : confirmPassword.displayError;
+    setConfirmPassword({ ...confirmPassword, ...{ focused, error, displayError } });
+  }
+
+  useEffect(() => setPasswordError(error), [ error ])
+  useEffect(() => setConfirmPasswordValue(confirmPassword.value), [ password ])
 
   const ref = useRef();
   const confirmRef = useRef();
@@ -64,36 +84,31 @@ export default function PasswordSection({ setValue, confirm = false }: PasswordS
       <div className="flex flex-col gap-2">
         <Typography
           variant="h6"
-          color={error && displayError ? "red" : "blue-gray"}>
+          color={ password.error && password.displayError ? "red" : "blue-gray" }>
           Password
         </Typography>
         <Input
-          inputRef={ref as MutableRefObject<any>}
-          name={`temp${Date.now()}`}
+          inputRef={ ref as MutableRefObject<any> }
+          name={ `temp${ Date.now() }` }
           type="number"
-          disabled={true}
+          disabled={ true }
           size="lg"
           label="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          value={password}
-          aria-invalid={error ? "true" : "false"}
-          error={error && displayError}
+          onChange={ (e) => setPasswordValue(e.target.value) }
+          onFocus={ () => setPasswordFocused(true) }
+          onBlur={ () => setPasswordFocused(false) }
+          value={ password.value }
+          aria-invalid={ password.error ? "true" : "false" }
+          error={ !!password.error && password.displayError }
           required
         />
-        <Typography
+        { password.error && password.displayError && <Typography
           variant="small"
           color="red"
-          className={classNames(
-            'flex items-center gap-1 font-normal',
-            error && displayError ? '' : 'hidden'
-          )}>
+          className="flex items-center gap-1 font-normal">
           <ExclamationCircleIcon className="w-1/12"/>
-          <span className="w-11/12">
-            Should be from 8 to 24 characters long, uppercase and lowercase letters, a number and a special character
-          </span>
-        </Typography>
+          <span className="w-11/12">{ password.error }</span>
+        </Typography> }
       </div>
 
       {
@@ -101,37 +116,32 @@ export default function PasswordSection({ setValue, confirm = false }: PasswordS
         <div className="flex flex-col gap-2">
           <Typography
             variant="h6"
-            color={confirmError && displayConfirmError ? "red" : "blue-gray"}>
+            color={ confirmPassword.error && confirmPassword.displayError ? "red" : "blue-gray" }>
             Confirm Password
           </Typography>
           <Input
-            inputRef={confirmRef as MutableRefObject<any>}
-            name={`temp${Date.now()}`}
+            inputRef={ confirmRef as MutableRefObject<any> }
+            name={ `temp${ Date.now() }` }
             type="number"
-            disabled={true}
+            disabled={ true }
             size="lg"
             label="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            onFocus={() => setConfirmFocused(true)}
-            onBlur={() => setConfirmFocused(false)}
-            value={confirmPassword}
-            aria-invalid={confirmError ? "true" : "false"}
+            onChange={ (e) => setConfirmPasswordValue(e.target.value) }
+            onFocus={ () => setConfirmPasswordFocused(true) }
+            onBlur={ () => setConfirmPasswordFocused(false) }
+            value={ confirmPassword.value }
+            aria-invalid={ confirmPassword.error ? "true" : "false" }
+            error={ !!confirmPassword.error && confirmPassword.displayError }
             aria-describedby="confirmnote"
-            error={confirmError && displayConfirmError}
             required
           />
-          <Typography
+          { confirmPassword.error && confirmPassword.displayError && <Typography
             variant="small"
             color="red"
-            className={classNames(
-              'flex items-center gap-1 font-normal',
-              confirmError && displayConfirmError ? '' : 'hidden'
-            )}>
+            className="flex items-center gap-1 font-normal">
             <ExclamationCircleIcon className="w-1/12"/>
-            <span className="w-11/12">
-              Should match the password
-            </span>
-          </Typography>
+            <span className="w-11/12">{ confirmPassword.error }</span>
+          </Typography> }
         </div>
       }
     </>
