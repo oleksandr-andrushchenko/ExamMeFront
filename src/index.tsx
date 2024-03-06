c927946a8848b434b4c56ba33d0b6df789864aa6
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-05 18:00:18 -0700

    non-func refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,6 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Categories from "./pages/Categories";
-import ThemeProvider from "./components/ThemeProvider";
-import Category from "./pages/Category";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import NotFound from "./pages/NotFound";
-import TermsAndConditions from "./pages/TermsAndConditions";
-import AuthProvider from "./context/AuthProvider";
-import AddCategory from "./pages/AddCategory";
-import RequireLoggedIn from "./components/RequireLoggedIn";
-import Permission from "./enum/Permission";
-import { default as Path } from "./enum/Route";
-import AddQuestion from "./pages/AddQuestion";
-import RequireLoggedOut from "./components/RequireLoggedOut";
+import App from './components/App';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={ <Layout/> }>
-      <Route path={ Path.HOME } element={ <Home/> }/>
-      <Route path={ Path.CATEGORIES } element={ <Categories/> }/>
-      <Route element={ <RequireLoggedIn permission={ Permission.CREATE_CATEGORY }/> }>
-        <Route path={ Path.ADD_CATEGORY } element={ <AddCategory/> }/>
-      </Route>
-      <Route path={ Path.CATEGORY } element={ <Category/> }/>
-      <Route element={ <RequireLoggedIn permission={ Permission.CREATE_QUESTION }/> }>
-        <Route path={ Path.ADD_QUESTION } element={ <AddQuestion/> }/>
-      </Route>
-      <Route element={ <RequireLoggedOut/> }>
-        <Route path={ Path.LOGIN } element={ <Login/> }/>
-      </Route>
-      <Route element={ <RequireLoggedOut/> }>
-        <Route path={ Path.REGISTER } element={ <Register/> }/>
-      </Route>
-      <Route path={ Path.TERMS_AND_CONDITIONS } element={ <TermsAndConditions/> }/>
-      <Route path="*" element={ <NotFound/> }/>
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <RouterProvider router={ router }/>
-      </ThemeProvider>
-    </AuthProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
