b4cd7ae7d2d8160fac11f2acac6bc5ce8ba8d4c9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-05 18:20:33 -0700

    soft non-func refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,18 @@
-import { Breadcrumbs, Typography } from "@material-tailwind/react";
-import { HomeIcon } from "@heroicons/react/24/solid";
-import React from "react";
-import { Link } from "react-router-dom";
-import Route from "../enum/Route";
+import { Breadcrumbs, Typography } from '@material-tailwind/react'
+import { HomeIcon } from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom'
+import Route from '../enum/Route'
+import { ReactNode } from 'react'
 
-export default () => {
-  return (
-    <>
-      <Breadcrumbs>
-        <Link to={ Route.HOME } className="flex items-center"><HomeIcon
-          className="inline-block w-4 h-4 mr-1"/> Home</Link>
-      </Breadcrumbs>
-      <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">
-        <HomeIcon className="inline-block h-8 w-8 mr-1"/> Home
-      </Typography>
-      <Typography variant="small" color="gray" className="mt-1 font-normal">
-        Latest activities
-      </Typography>
-    </>
-  );
-}
+export default (): ReactNode => <>
+  <Breadcrumbs>
+    <Link to={ Route.HOME } className="flex items-center"><HomeIcon
+      className="inline-block w-4 h-4 mr-1"/> Home</Link>
+  </Breadcrumbs>
+  <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">
+    <HomeIcon className="inline-block h-8 w-8 mr-1"/> Home
+  </Typography>
+  <Typography variant="small" color="gray" className="mt-1 font-normal">
+    Latest activities
+  </Typography>
+</>
