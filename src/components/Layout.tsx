21aa3830e2e6f46a050fd872d44ee4b06a978cb5
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-05 18:16:32 -0700

    non-func refactoring

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,10 @@
-import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import NavBar from './NavBar';
+import { Outlet } from 'react-router-dom';
+import { ReactNode } from 'react';
 
-export default function Layout() {
-  return (
-    <div className="min-h-full">
-      <NavBar/>
-      <main className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
-        <Outlet/>
-      </main>
-    </div>
-  )
-};
+export default (): ReactNode => <div className="min-h-full">
+  <NavBar/>
+  <main className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
+    <Outlet/>
+  </main>
+</div>
