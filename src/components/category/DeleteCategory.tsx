529f1f30e3d0b105cc3b00ab142b9f7ecfae46e9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-10 22:23:37 -0600

    delete category component added

---
+++ src/components/category/DeleteCategory.tsx
@@ -0,0 +1,81 @@
+import { Button, Dialog, DialogBody, DialogFooter, DialogHeader, Typography } from '@material-tailwind/react'
+import { ExclamationCircleIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import React, { useEffect, useState } from 'react'
+import Route from '../../enum/Route.ts'
+import { useNavigate } from 'react-router-dom'
+import Category from '../../schema/Category.ts'
+import deleteCategory from '../../api/category/deleteCategory.ts'
+import normalizeApiErrors from '../../utils/normalizeApiErrors.ts'
+
+interface Props {
+  category: Category,
+}
+
+export default ({ category }: Props) => {
+  const [ open, setOpen ] = useState<boolean>(false)
+  const [ processing, setProcessing ] = useState<boolean>(false)
+  const handleOpen = () => setOpen(!open)
+  const [ error, setError ] = useState<string>('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (processing) {
+      deleteCategory(category.id)
+        .then(() => navigate(Route.CATEGORIES, { replace: true }))
+        .catch((error) => {
+          const errors = normalizeApiErrors(error)
+          console.log(errors)
+          setError(errors?.unknown || '')
+        })
+        .finally(() => setProcessing(false))
+    }
+  }, [ processing ])
+
+  return <>
+    <Button
+      size="sm"
+      className="rounded capitalize font-normal"
+      onClick={ handleOpen }
+      disabled={ processing }>
+      <XMarkIcon className="inline-block h-4 w-4"/> { processing ? 'Deleting Category...' : 'Delete Category' }
+    </Button>
+    <Dialog size="sm" open={ open } handler={ handleOpen }>
+      <DialogHeader>
+        <Typography variant="h4" color="blue-gray">
+          Are you sure you want to delete "{ category.name }" category?
+        </Typography>
+      </DialogHeader>
+      <DialogBody divider>
+        <Typography className="font-normal">
+          This will delete "{ category.name }" <b>category</b> and <b>all</b> its <b>questions
+          ({ category.questionCount })</b> permanently.
+          <br/>
+          You cannot undo this action.
+        </Typography>
+
+        { error && <Typography
+          color="red"
+          className="flex items-center gap-1">
+          <ExclamationCircleIcon className="inline-block h-5 w-5"/> { error }
+        </Typography> }
+      </DialogBody>
+      <DialogFooter className="space-x-2">
+        <Button
+          size="sm"
+          className="rounded capitalize font-normal"
+          onClick={ handleOpen }>
+          Cancel
+        </Button>
+
+        <Button
+          size="md"
+          color="red"
+          className="rounded capitalize font-normal"
+          onClick={ () => setProcessing(true) }
+          disabled={ processing }>
+          <XMarkIcon className="inline-block h-4 w-4"/> { processing ? 'Deleting...' : 'Delete' }
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  </>
+}
