8d69d1ef6967f3542ee609edb593b891dba8f57f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-10 22:24:59 -0600

    delete category component instead of raw button

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,7 +1,7 @@
-import { Link, Params, useNavigate, useParams } from 'react-router-dom'
+import { Link, Params, useParams } from 'react-router-dom'
 import { Breadcrumbs, Button, List, ListItem, Typography } from '@material-tailwind/react'
 import Route from '../enum/Route'
-import { CubeIcon, ExclamationCircleIcon, HomeIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/solid'
+import { CubeIcon, HomeIcon, PlusIcon } from '@heroicons/react/24/solid'
 import React, { ReactNode, useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import Permission from '../enum/Permission'
@@ -10,8 +10,7 @@ import Category from '../schema/Category'
 import Question from '../schema/Question'
 import getCategory from '../api/category/getCategory'
 import getCategoryQuestions from '../api/question/getCategoryQuestions'
-import deleteCategory from '../api/category/deleteCategory'
-import normalizeApiErrors from '../utils/normalizeApiErrors'
+import DeleteCategory from '../components/category/DeleteCategory'
 
 interface Data {
   category: Category | undefined,
@@ -21,28 +20,12 @@ interface Data {
 export default (): ReactNode => {
   const { categoryId } = useParams<Params>() as { categoryId: string }
   const [ { category, questions }, setData ] = useState<Data>({ category: undefined, questions: undefined })
-  const [ deletingCategory, setDeletingCategory ] = useState<boolean>(false)
-  const [ error, setError ] = useState<string>('')
   const { auth, me, checkAuth } = useAuth()
-  const navigate = useNavigate()
 
   useEffect((): void => {
     Promise.all<any>([ getCategory(categoryId), getCategoryQuestions(categoryId) ])
       .then(([ category, questions ]): void => setData({ category, questions }))
   }, [])
-
-  useEffect(() => {
-    if (deletingCategory) {
-      deleteCategory(categoryId)
-        .then(() => navigate(Route.CATEGORIES, { replace: true }))
-        .catch((error) => {
-          const errors = normalizeApiErrors(error)
-          console.log(errors)
-          setError(errors?.unknown || '')
-        })
-        .finally(() => setDeletingCategory(false))
-    }
-  }, [ deletingCategory ])
 
   return <>
     <Breadcrumbs>
@@ -74,27 +57,16 @@ export default (): ReactNode => {
       }) }
     </List> }
 
-    { error && <Typography
-      color="red"
-      className="flex items-center gap-1">
-      <ExclamationCircleIcon className="inline-block h-4 w-4"/> { error }
-    </Typography> }
-
     { auth && me === undefined ? <Spinner/> : checkAuth(Permission.CREATE_QUESTION) && <Link
       to={ Route.ADD_QUESTION.replace(':categoryId', categoryId) }>
       <Button
         size="sm"
-        className="rounded capitalize font-normal mt-3">
+        className="rounded capitalize font-normal">
         <PlusIcon className="inline-block h-4 w-4"/> Add Question
       </Button>
     </Link> }
 
-    { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_CATEGORY) && <Button
-      size="sm"
-      className="rounded capitalize font-normal mt-3"
-      onClick={ () => setDeletingCategory(true) }
-      disabled={ deletingCategory }>
-      <MinusIcon className="inline-block h-4 w-4"/> { deletingCategory ? 'Removing Category...' : 'Remove Category' }
-    </Button> }
+    { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_CATEGORY) &&
+      (category === undefined ? <Spinner/> : <DeleteCategory category={ category }/>) }
   </>
 }
