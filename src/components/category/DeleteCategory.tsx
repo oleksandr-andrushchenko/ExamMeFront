d19f0333902a5381e39a4ea31aeec5b446fb8d42
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-10 22:57:02 -0600

    styles updated

--- src/components/category/DeleteCategory.tsx
+++ src/components/category/DeleteCategory.tsx
@@ -1,11 +1,11 @@
-import { Button, Dialog, DialogBody, DialogFooter, DialogHeader, Typography } from '@material-tailwind/react'
+import { Button, Card, CardBody, CardFooter, Dialog, Typography } from '@material-tailwind/react'
 import { ExclamationCircleIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import React, { useEffect, useState } from 'react'
 import Route from '../../enum/Route.ts'
 import { useNavigate } from 'react-router-dom'
-import Category from '../../schema/Category.ts'
-import deleteCategory from '../../api/category/deleteCategory.ts'
-import normalizeApiErrors from '../../utils/normalizeApiErrors.ts'
+import Category from '../../schema/Category'
+import deleteCategory from '../../api/category/deleteCategory'
+import normalizeApiErrors from '../../utils/normalizeApiErrors'
 
 interface Props {
   category: Category,
@@ -34,48 +34,50 @@ export default ({ category }: Props) => {
   return <>
     <Button
       size="sm"
+      color="red"
       className="rounded capitalize font-normal"
       onClick={ handleOpen }
       disabled={ processing }>
       <XMarkIcon className="inline-block h-4 w-4"/> { processing ? 'Deleting Category...' : 'Delete Category' }
     </Button>
-    <Dialog size="sm" open={ open } handler={ handleOpen }>
-      <DialogHeader>
-        <Typography variant="h4" color="blue-gray">
-          Are you sure you want to delete "{ category.name }" category?
-        </Typography>
-      </DialogHeader>
-      <DialogBody divider>
-        <Typography className="font-normal">
-          This will delete "{ category.name }" <b>category</b> and <b>all</b> its <b>questions
-          ({ category.questionCount })</b> permanently.
-          <br/>
-          You cannot undo this action.
-        </Typography>
+    <Dialog size="xs" open={ open } handler={ handleOpen } className="bg-transparent shadow-none">
+      <Card className="mx-auto w-full max-w-[24rem] text-center">
+        <CardBody className="flex flex-col gap-4">
+          <Typography variant="h4" color="blue-gray">
+            Are you sure you want to delete "{ category.name }" category?
+          </Typography>
+          <Typography
+            className="mb-3 font-normal"
+            variant="paragraph"
+            color="gray"
+          >
+            This will delete "{ category.name }" category and all its questions [ { category.questionCount } ]
+            permanently.
+            <br/>
+            You cannot undo this action.
+          </Typography>
+          { error && <Typography color="red">
+            <ExclamationCircleIcon className="inline-block h-5 w-5"/> { error }
+          </Typography> }
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button
+            size="sm"
+            className="rounded capitalize font-normal"
+            onClick={ handleOpen }>
+            Cancel
+          </Button>
 
-        { error && <Typography
-          color="red"
-          className="flex items-center gap-1">
-          <ExclamationCircleIcon className="inline-block h-5 w-5"/> { error }
-        </Typography> }
-      </DialogBody>
-      <DialogFooter className="space-x-2">
-        <Button
-          size="sm"
-          className="rounded capitalize font-normal"
-          onClick={ handleOpen }>
-          Cancel
-        </Button>
-
-        <Button
-          size="md"
-          color="red"
-          className="rounded capitalize font-normal"
-          onClick={ () => setProcessing(true) }
-          disabled={ processing }>
-          <XMarkIcon className="inline-block h-4 w-4"/> { processing ? 'Deleting...' : 'Delete' }
-        </Button>
-      </DialogFooter>
+          <Button
+            size="md"
+            color="red"
+            className="rounded capitalize font-normal ml-1"
+            onClick={ () => setProcessing(true) }
+            disabled={ processing }>
+            <XMarkIcon className="inline-block h-4 w-4"/> { processing ? 'Deleting...' : 'Delete' }
+          </Button>
+        </CardFooter>
+      </Card>
     </Dialog>
   </>
 }
