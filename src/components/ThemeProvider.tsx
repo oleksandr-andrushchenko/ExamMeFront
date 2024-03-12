68961bf43bb3a2d118c9f0d964bde959c75b57cc
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-12 15:17:12 -0600

    button styles added

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,7 +1,29 @@
 import { ThemeProvider as TailwindThemeProvider } from '@material-tailwind/react'
 import { ReactNode } from 'react'
 
-const theme = {}
+const theme = {
+  button: {
+    styles: {
+      base: {
+        initial: {
+          fontWeight: 'font-normal',
+          textTransform: 'capitalize',
+        },
+      },
+      sizes: {
+        sm: {
+          borderRadius: 'rounded',
+        },
+        md: {
+          borderRadius: 'rounded',
+        },
+        lg: {
+          borderRadius: 'rounded',
+        },
+      },
+    },
+  },
+}
 
 export default ({ children }: { children: any }): ReactNode => <TailwindThemeProvider value={ theme }>
   { children }
