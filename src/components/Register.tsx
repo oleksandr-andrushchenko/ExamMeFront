4c11b9a82c6bd341fdb4fd6bc8e751328a1977de
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-16 16:58:08 -0600

    dialog removed

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,7 @@
-import { Button, Card, CardBody, Checkbox, Dialog, Typography } from '@material-tailwind/react'
-import { ExclamationCircleIcon, UserPlusIcon } from '@heroicons/react/24/solid'
+import { Button, Checkbox, Typography } from '@material-tailwind/react'
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid'
 import React, { ReactNode, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import EmailSection from './EmailSection'
 import PasswordSection from './PasswordSection'
 import useAuth from '../hooks/useAuth'
@@ -11,13 +11,13 @@ import Route from '../enum/Route'
 import postMe from '../api/me/postMe'
 
 interface Props {
+  onSubmit: () => void,
+
+  buttons?: ReactNode,
 }
 
-export default ({}: Props): ReactNode => {
-  const [ open, setOpen ] = useState<boolean>(false)
+export default ({ buttons, onSubmit }: Props): ReactNode => {
   const [ processing, setProcessing ] = useState<boolean>(false)
-  const handleOpen = () => setOpen(!open)
-  const navigate = useNavigate()
   const [ email, setEmail ] = useState<string>('')
   const [ emailError, setEmailError ] = useState<string>('')
   const [ password, setPassword ] = useState<string>('')
@@ -34,7 +34,7 @@ export default ({}: Props): ReactNode => {
     try {
       await postMe({ email, password })
       setAuth(await postAuth({ email, password }))
-      navigate(0)
+      onSubmit()
     } catch (err) {
       const errors = normalizeApiErrors(err)
       console.log(errors)
@@ -46,66 +46,45 @@ export default ({}: Props): ReactNode => {
     }
   }
 
-  return <>
-    <Button
-      size="sm"
-      onClick={ handleOpen }
-      disabled={ processing }>
-      <UserPlusIcon className="inline-block h-4 w-4"/> Register
-    </Button>
-    <Dialog size="xs" open={ open } handler={ handleOpen } className="bg-transparent shadow-none">
-      <Card>
-        <CardBody className="flex flex-col gap-4">
-          <Typography variant="h4" color="blue-gray">Register</Typography>
-          <form className="flex flex-col gap-6" onSubmit={ handleSubmit }
-                method="post">
+  return <form className="flex flex-col gap-6" onSubmit={ handleSubmit } method="post">
+    <Typography variant="h4" color="blue-gray">Register</Typography>
 
-            <EmailSection setValue={ setEmail as any } error={ emailError } focus/>
-            <PasswordSection setValue={ setPassword as any } error={ passwordError } confirm/>
+    <EmailSection setValue={ setEmail as any } error={ emailError } focus/>
+    <PasswordSection setValue={ setPassword as any } error={ passwordError } confirm/>
 
-            <div className="-mt-4">
-              <Checkbox
-                label={
-                  <Typography
-                    variant="small"
-                    color="gray"
-                    className="flex items-center font-normal">
-                    I agree the
-                    <Link to={ Route.TERMS_AND_CONDITIONS }>Terms and Conditions</Link>
-                  </Typography>
-                }
-                onChange={ (e) => setTerms(e.target.checked) }
-                required
-              />
-            </div>
+    <div className="-mt-4">
+      <Checkbox
+        label={
+          <Typography
+            variant="small"
+            color="gray"
+            className="flex items-center font-normal">
+            I agree the
+            <Link to={ Route.TERMS_AND_CONDITIONS }>Terms and Conditions</Link>
+          </Typography>
+        }
+        onChange={ (e) => setTerms(e.target.checked) }
+        required
+      />
+    </div>
 
-            { error && <Typography
-              variant="small"
-              color="red"
-              className="flex items-center gap-1 font-normal">
-              <ExclamationCircleIcon className="w-1/12"/>
-              <span className="w-11/12">{ error }</span>
-            </Typography> }
+    { error && <Typography
+      variant="small"
+      color="red"
+      className="flex items-center gap-1 font-normal">
+      <ExclamationCircleIcon className="w-1/12"/>
+      <span className="w-11/12">{ error }</span>
+    </Typography> }
 
-            <div>
-              <Button
-                size="sm"
-                type="reset"
-                onClick={ handleOpen }>
-                Cancel
-              </Button>
-
-              <Button
-                size="md"
-                type="submit"
-                className="ml-1"
-                disabled={ !email || !password || !terms || processing }>
-                { processing ? 'Registering in...' : 'Register' }
-              </Button>
-            </div>
-          </form>
-        </CardBody>
-      </Card>
-    </Dialog>
-  </>
+    <div>
+      { buttons }
+      <Button
+        size="md"
+        type="submit"
+        className="ml-1"
+        disabled={ !email || !password || !terms || processing }>
+        { processing ? 'Registering in...' : 'Register' }
+      </Button>
+    </div>
+  </form>
 }
