535fb69aa2087321f601372e2a367697d0109a42
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-16 15:51:44 -0600

    register route removed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,10 @@ import Home from '../pages/Home'
 import Categories from '../pages/Categories'
 import ThemeProvider from './ThemeProvider'
 import Category from '../pages/Category'
-import Register from '../pages/Register'
 import NotFound from '../pages/NotFound'
 import TermsAndConditions from '../pages/TermsAndConditions'
 import AuthProvider from '../context/AuthProvider'
 import { default as Path } from '../enum/Route'
-import RequireLoggedOut from './RequireLoggedOut'
 import { ReactNode } from 'react'
 import Question from '../pages/Question'
 
@@ -19,9 +17,6 @@ const routes = <Routes>
     <Route path={ Path.CATEGORIES } element={ <Categories/> }/>
     <Route path={ Path.CATEGORY } element={ <Category/> }/>
     <Route path={ Path.QUESTION } element={ <Question/> }/>
-    <Route element={ <RequireLoggedOut/> }>
-      <Route path={ Path.REGISTER } element={ <Register/> }/>
-    </Route>
     <Route path={ Path.TERMS_AND_CONDITIONS } element={ <TermsAndConditions/> }/>
     <Route path="*" element={ <NotFound/> }/>
   </Route>
