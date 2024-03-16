b1288dccd76bde47c64806dac21080c94c464bfc
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-16 12:29:30 -0600

    h1 icon removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,7 @@ export default (): ReactNode => <>
     <Link to={ Route.HOME } className="flex items-center"><HomeIcon
       className="inline-block w-4 h-4 mr-1"/> Home</Link>
   </Breadcrumbs>
-  <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">
-    <HomeIcon className="inline-block h-8 w-8 mr-1"/> Home
-  </Typography>
+  <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">Home</Typography>
   <Typography variant="small" color="gray" className="mt-1 font-normal">
     Latest activities
   </Typography>
