b398e3bb1210f6bea8ec01363a07ad2bca1d5147
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-16 12:34:31 -0600

    create, update and delete buttons added; soft non-func refactoring

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,22 +1,26 @@
 import { Link } from 'react-router-dom'
-import { Breadcrumbs, Card, Chip, List, ListItem, Typography } from '@material-tailwind/react'
+import { Breadcrumbs, Card, Chip, List, ListItem, ListItemSuffix, Typography } from '@material-tailwind/react'
 import Category from '../schema/Category'
 import Route from '../enum/Route'
 import useAuth from '../hooks/useAuth'
-import { HomeIcon, Squares2X2Icon } from '@heroicons/react/24/solid'
-import { ReactNode, useEffect, useState } from 'react'
+import { HomeIcon } from '@heroicons/react/24/solid'
+import React, { ReactNode, useEffect, useState } from 'react'
 import Permission from '../enum/Permission'
 import Spinner from '../components/Spinner'
 import getCategories from '../api/category/getCategories'
 import AddCategory from '../components/category/AddCategory'
+import AddQuestion from '../components/question/AddQuestion'
+import DeleteCategory from '../components/category/DeleteCategory'
 
 export default (): ReactNode => {
   const [ categories, setCategories ] = useState<Category[] | undefined>(undefined)
   const { auth, me, checkAuth } = useAuth()
 
-  useEffect((): void => {
+
+  const refresh = (): void => {
     getCategories().then((categories): void => setCategories(categories))
-  }, [])
+  }
+  useEffect(refresh, [])
 
   return <>
     <Breadcrumbs>
@@ -24,9 +28,7 @@ export default (): ReactNode => {
         className="inline-block w-4 h-4 mr-1"/> Home</Link>
       <Link to={ Route.CATEGORIES }>Categories</Link>
     </Breadcrumbs>
-    <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">
-      <Squares2X2Icon className="inline-block h-8 w-8 mr-1"/> Categories
-    </Typography>
+    <Typography variant="h1" color="blue-gray" className="flex items-baseline mt-1">Categories</Typography>
     <Typography variant="small" color="gray" className="mt-1 font-normal">
       Available categories
     </Typography>
@@ -41,11 +43,24 @@ export default (): ReactNode => {
               variant="ghost"
               value={ index + 1 }
               className="rounded-full inline-block"
-            /> { category.name } <Chip
-            value={ category.questionCount }
-            className="rounded-full inline-block"
-          />
+            /> { category.name }
           </Link>
+
+          <ListItemSuffix>
+            <Chip
+              value={ category.questionCount }
+              className="rounded-full inline-block"
+            />
+
+            { auth && me === undefined ? <Spinner/> : checkAuth(Permission.CREATE_QUESTION) &&
+              <AddQuestion category={ category } onSubmit={ refresh }/> }
+
+            { auth && me === undefined ? <Spinner/> : checkAuth(Permission.UPDATE_CATEGORY) &&
+              <AddCategory category={ category } onSubmit={ refresh }/> }
+
+            { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_CATEGORY) &&
+              <DeleteCategory category={ category } onSubmit={ refresh }/> }
+          </ListItemSuffix>
         </ListItem>) }
       </List>
     </Card> }
