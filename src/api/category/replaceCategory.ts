067e8cae1fa0139a546b438faf437a49067f28e7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-16 12:30:34 -0600

    soft non-func refactoring

--- src/api/category/replaceCategory.ts
+++ src/api/category/replaceCategory.ts
@@ -1,5 +1,5 @@
 import client from '../client'
-import CategoryTransfer from '../../schema/CategoryTransfer.ts'
+import CategoryTransfer from '../../schema/CategoryTransfer'
 import { AxiosResponse } from 'axios'
 
 export default async (id: string, transfer: CategoryTransfer): Promise<AxiosResponse> => await client.put(`/categories/${ id }`, transfer)
