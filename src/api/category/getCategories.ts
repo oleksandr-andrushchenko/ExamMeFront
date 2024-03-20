b6af15b7627c5483a7ba23bff73fb35458c3f4de
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-20 17:02:35 -0600

    cursor params added

--- src/api/category/getCategories.ts
+++ src/api/category/getCategories.ts
@@ -1,4 +1,10 @@
 import client from '../client'
 import Category from '../../schema/Category'
+import { AxiosRequestConfig } from 'axios'
 
-export default async (): Promise<Category[]> => (await client.get('/categories')).data
+export default async (cursor: string = 'id', size: number = 20, order: 'asc' | 'desc' = 'desc'): Promise<Category[]> => {
+  const params = { cursor, size, order }
+  const response = await client.get('/categories', { params } as AxiosRequestConfig)
+
+  return response.data.data
+}
--- src/api/question/getCategoryQuestions.ts
+++ src/api/question/getCategoryQuestions.ts
@@ -1,4 +1,10 @@
 import client from '../client'
 import Question from '../../schema/Question'
+import { AxiosRequestConfig } from 'axios'
 
-export default async (categoryId: string): Promise<Question[]> => (await client.get(`/categories/${ categoryId }/questions`)).data
+export default async (categoryId: string, cursor: string = 'id', size: number = 20, order: 'asc' | 'desc' = 'desc'): Promise<Question[]> => {
+  const params = { cursor, size, order }
+  const response = await client.get(`/categories/${ categoryId }/questions`, { params } as AxiosRequestConfig)
+
+  return response.data.data
+}
