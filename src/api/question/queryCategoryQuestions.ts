b73daa8b0c607140bc77512bbeff7477e8e0a772
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-27 20:42:14 -0600

    types fixed

--- src/api/question/queryCategoryQuestions.ts
+++ src/api/question/queryCategoryQuestions.ts
@@ -3,4 +3,8 @@ import Question from '../../schema/Question'
 import Pagination from '../../types/pagination/Pagination'
 import Paginated from '../../types/pagination/Paginated'
 
-export default async (categoryId: string, pagination: Pagination = {}): Promise<Paginated<Question>> => (await client.get(`/categories/${ categoryId }/questions`, { params: pagination })).data
+export default async (categoryId: string, pagination: Pagination | URLSearchParams = {}): Promise<Paginated<Question>> => {
+  const config = { params: pagination }
+
+  return (await client.get(`/categories/${ categoryId }/questions`, config)).data
+}
