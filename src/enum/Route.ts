d1a14d38533b3bc9719b633b97a691ac0760ca76
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-31 15:12:06 -0600

    exams routes added

--- src/enum/Route.ts
+++ src/enum/Route.ts
@@ -4,6 +4,8 @@ enum Route {
   CATEGORY = '/categories/:categoryId',
   QUESTIONS = '/questions',
   QUESTION = '/categories/:categoryId/questions/:questionId',
+  EXAMS = '/exams',
+  EXAM = '/categories/:categoryId/exams/:examId',
   LOGIN = '/login',
   REGISTER = '/register',
   TERMS_AND_CONDITIONS = '/terms-and-conditions',
