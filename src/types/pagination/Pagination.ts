a219befae5030ec97b3a9034f9db6caba86f966d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-31 21:02:57 -0600

    size either as string or number

--- src/types/pagination/Pagination.ts
+++ src/types/pagination/Pagination.ts
@@ -2,6 +2,6 @@ export default class Pagination {
   prevCursor?: string
   nextCursor?: string
   cursor?: string = 'id'
-  size?: string = '20'
+  size?: string | number = '20'
   order?: 'asc' | 'desc' = 'desc'
 }
