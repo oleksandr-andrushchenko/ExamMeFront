a24c40f443961a14085f8d0b3b92b0342162eebf
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-08 16:49:25 -0600

    synced with latest api changes

--- src/api/exam/getExamQuestion.ts
+++ src/api/exam/getExamQuestion.ts
@@ -1,6 +1,6 @@
 import client from '../client'
 import ExamQuestion from '../../schema/exam/ExamQuestion'
 
-export default async (examId: string, questionId: string): Promise<ExamQuestion> => {
-  return (await client.get(`/exams/${ examId }/question/${ questionId }`)).data
+export default async (examId: string, question: number): Promise<ExamQuestion> => {
+  return (await client.get(`/exams/${ examId }/questions/${ question }`)).data
 }
