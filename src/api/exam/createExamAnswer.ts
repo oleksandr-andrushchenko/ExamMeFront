6a4920ccb408cb849c1568b5104618a97b95c34c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-08 16:48:23 -0600

    renamed

--- src/api/exam/createExamAnswer.ts
+++
@@ -1,6 +0,0 @@
-import client from '../client'
-import ExamQuestion from '../../schema/exam/ExamQuestion'
-
-export default async (examId: string, questionId: string, answer: string | number): Promise<ExamQuestion> => {
-  return (await client.post(`/exams/${ examId }/question/${ questionId }/answer`, { answer })).data
-}
