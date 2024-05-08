86a69f636c69cade6c3bc369fb19c58661841cb2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-08 16:47:53 -0600

    deleted

--- src/api/exam/getCurrentExamQuestion.ts
+++
@@ -1,6 +0,0 @@
-import client from '../client'
-import ExamQuestion from '../../schema/exam/ExamQuestion'
-
-export default async (examId: string): Promise<ExamQuestion> => {
-  return (await client.get(`/exams/${ examId }/current-question`)).data
-}
