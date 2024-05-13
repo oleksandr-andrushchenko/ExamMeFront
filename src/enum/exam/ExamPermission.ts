acac9161344e26b6974b4d2b36333e4f65819b45
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-13 03:49:47 -0600

    synced with backend

--- src/enum/exam/ExamPermission.ts
+++ src/enum/exam/ExamPermission.ts
@@ -1,7 +1,10 @@
 enum ExamPermission {
   CREATE = 'createExam',
   GET = 'getExam',
-  DELETE = 'deleteExam'
+  GET_QUESTION = 'getExamQuestion',
+  CREATE_QUESTION_ANSWER = 'createExamQuestionAnswer',
+  CREATE_COMPLETION = 'createExamCompletion',
+  DELETE = 'deleteExam',
 }
 
 export default ExamPermission
