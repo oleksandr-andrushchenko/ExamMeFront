6ec8a0474d3cc22138415eb2b24438b22a0fac98
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-19 00:54:58 -0600

    soft non-func refactoring

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -112,194 +112,196 @@ export default function Category(): ReactNode {
     document.title = category?.name || 'ExamMe'
   }, [ category ])
 
-  return <>
-    <Breadcrumbs>
-      <Link to={ Route.HOME } className="flex items-center"><HomeIcon className="w-4 h-4 mr-1"/> Home</Link>
-      <Link to={ Route.CATEGORIES }>Categories</Link>
-      { category === undefined ? <Spinner/> :
-        <Link to={ Route.CATEGORY.replace(':categoryId', category.id!) }>{ category.name }</Link> }
-    </Breadcrumbs>
+  return (
+    <>
+      <Breadcrumbs>
+        <Link to={ Route.HOME } className="flex items-center"><HomeIcon className="w-4 h-4 mr-1"/> Home</Link>
+        <Link to={ Route.CATEGORIES }>Categories</Link>
+        { category === undefined ? <Spinner/> :
+          <Link to={ Route.CATEGORY.replace(':categoryId', category.id!) }>{ category.name }</Link> }
+      </Breadcrumbs>
 
-    <Typography as="h1" variant="h2" className="mt-1">{ category === undefined ?
-      <Spinner/> : category.name }</Typography>
+      <Typography as="h1" variant="h2" className="mt-1">{ category === undefined ?
+        <Spinner/> : category.name }</Typography>
 
-    <Rating/>
+      <Rating/>
 
-    <Typography variant="small" className="mt-1">Category info</Typography>
+      <Typography variant="small" className="mt-1">Category info</Typography>
 
-    { error && <Typography
-      variant="small"
-      color="red"
-      className="flex items-center gap-1 font-normal">
-      <ExclamationCircleIcon className="w-1/12"/>
-      <span className="w-11/12">{ error }</span>
-    </Typography> }
+      { error && <Typography
+        variant="small"
+        color="red"
+        className="flex items-center gap-1 font-normal">
+        <ExclamationCircleIcon className="w-1/12"/>
+        <span className="w-11/12">{ error }</span>
+      </Typography> }
 
-    <div className="flex gap-1 items-center mt-4">
-      { auth && me === undefined ? <Spinner/> : checkAuth(Permission.CREATE_QUESTION) && (category === undefined ?
-        <Spinner/> : <AddQuestion category={ category }/>) }
+      <div className="flex gap-1 items-center mt-4">
+        { auth && me === undefined ? <Spinner/> : checkAuth(Permission.CREATE_QUESTION) && (category === undefined ?
+          <Spinner/> : <AddQuestion category={ category }/>) }
 
-      { auth && me === undefined ? <Spinner/> : checkAuth(Permission.UPDATE_CATEGORY) &&
-        (category === undefined ? <Spinner/> :
-          <AddCategory category={ category }
-                       onSubmit={ (category: Category): void => setCategory(category) }/>) }
+        { auth && me === undefined ? <Spinner/> : checkAuth(Permission.UPDATE_CATEGORY) &&
+          (category === undefined ? <Spinner/> :
+            <AddCategory category={ category }
+                         onSubmit={ (category: Category): void => setCategory(category) }/>) }
 
-      { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_CATEGORY) &&
-        (category === undefined ? <Spinner/> : <DeleteCategory category={ category }/>) }
+        { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_CATEGORY) &&
+          (category === undefined ? <Spinner/> : <DeleteCategory category={ category }/>) }
 
-      { category === undefined ? <Spinner/> : (category.questionCount > 0) && <AddExam category={ category }/> }
-    </div>
+        { category === undefined ? <Spinner/> : (category.questionCount > 0) && <AddExam category={ category }/> }
+      </div>
 
-    <div className="flex gap-1 items-center mt-4">
-      <Tabs value="all" className="min-w-[170px]">
-        <TabsHeader>
-          { tableFilters.map((value): ReactNode => (
-            <Tab key={ value } value={ value } className="text-xs small text-small capitalize"
-                 onClick={ (): void => applySearchParams({ price: value === 'all' ? undefined : value }) }>
-              { value }
-            </Tab>
+      <div className="flex gap-1 items-center mt-4">
+        <Tabs value="all" className="min-w-[170px]">
+          <TabsHeader>
+            { tableFilters.map((value): ReactNode => (
+              <Tab key={ value } value={ value } className="text-xs small text-small capitalize"
+                   onClick={ (): void => applySearchParams({ price: value === 'all' ? undefined : value }) }>
+                { value }
+              </Tab>
+            )) }
+          </TabsHeader>
+        </Tabs>
+
+        <Select
+          label="Difficulty"
+          onChange={ (difficulty: string): void => applySearchParams({ difficulty }) }
+          value={ searchParams.get('difficulty') || '' }
+          className="capitalize">
+          { Object.values(QuestionDifficulty)
+            .map((difficulty: string): ReactNode => (
+              <Option key={ difficulty }
+                      value={ difficulty }
+                      disabled={ difficulty === searchParams.get('difficulty') }
+                      className="capitalize">{ difficulty }</Option>
+            )) }
+        </Select>
+
+        <Select
+          label="Type"
+          onChange={ (type: string): void => applySearchParams({ type }) }
+          value={ searchParams.get('type') || '' }
+          className="capitalize">
+          { Object.values(QuestionType)
+            .map((type: string): ReactNode => (
+              <Option key={ type }
+                      value={ type }
+                      disabled={ type === searchParams.get('type') }
+                      className="capitalize">{ type }</Option>
+            )) }
+        </Select>
+
+        <Input
+          label="Search"
+          value={ searchParams.get('search') || '' }
+          onChange={ (e: React.ChangeEvent<HTMLInputElement>): void => applySearchParams({ search: e.target.value }) }
+          icon={ <MagnifyingGlassIcon className="h-4 w-4"/> }/>
+
+        <Select
+          label="Size"
+          onChange={ (size: string): void => applySearchParams({ size }) }
+          value={ searchParams.get('size') || '' }
+          className="capitalize">
+          { [ 1, 5, 10, 20, 30, 40, 50 ].map((size: number): ReactNode => (
+            <Option key={ size }
+                    value={ `${ size }` }
+                    disabled={ `${ size }` === searchParams.get('size') }>{ size }</Option>
           )) }
-        </TabsHeader>
-      </Tabs>
+        </Select>
 
-      <Select
-        label="Difficulty"
-        onChange={ (difficulty: string): void => applySearchParams({ difficulty }) }
-        value={ searchParams.get('difficulty') || '' }
-        className="capitalize">
-        { Object.values(QuestionDifficulty)
-          .map((difficulty: string): ReactNode => (
-            <Option key={ difficulty }
-                    value={ difficulty }
-                    disabled={ difficulty === searchParams.get('difficulty') }
-                    className="capitalize">{ difficulty }</Option>
-          )) }
-      </Select>
+        { questions === undefined ? <Spinner/> : ((questions.meta.prevCursor || questions.meta.nextCursor) &&
+          <ButtonGroup variant="outlined">
+            { questions.meta.prevCursor && (
+              <IconButton onClick={ (): void => applySearchParams({ prevCursor: questions?.meta.prevCursor }) }>
+                <ArrowLeftIcon className="w-4 h-4"/>
+              </IconButton>
+            ) }
+            { questions.meta.nextCursor && (
+              <IconButton onClick={ (): void => applySearchParams({ nextCursor: questions?.meta.nextCursor }) }>
+                <ArrowRightIcon className="w-4 h-4"/>
+              </IconButton>
+            ) }
+          </ButtonGroup>) }
 
-      <Select
-        label="Type"
-        onChange={ (type: string): void => applySearchParams({ type }) }
-        value={ searchParams.get('type') || '' }
-        className="capitalize">
-        { Object.values(QuestionType)
-          .map((type: string): ReactNode => (
-            <Option key={ type }
-                    value={ type }
-                    disabled={ type === searchParams.get('type') }
-                    className="capitalize">{ type }</Option>
-          )) }
-      </Select>
+        { showClear() && <div>
+          <Button variant="outlined" onClick={ clearSearchParams }>Clear</Button>
+        </div> }
+      </div>
 
-      <Input
-        label="Search"
-        value={ searchParams.get('search') || '' }
-        onChange={ (e: React.ChangeEvent<HTMLInputElement>): void => applySearchParams({ search: e.target.value }) }
-        icon={ <MagnifyingGlassIcon className="h-4 w-4"/> }/>
-
-      <Select
-        label="Size"
-        onChange={ (size: string): void => applySearchParams({ size }) }
-        value={ searchParams.get('size') || '' }
-        className="capitalize">
-        { [ 1, 5, 10, 20, 30, 40, 50 ].map((size: number): ReactNode => (
-          <Option key={ size }
-                  value={ `${ size }` }
-                  disabled={ `${ size }` === searchParams.get('size') }>{ size }</Option>
-        )) }
-      </Select>
-
-      { questions === undefined ? <Spinner/> : ((questions.meta.prevCursor || questions.meta.nextCursor) &&
-        <ButtonGroup variant="outlined">
-          { questions.meta.prevCursor && (
-            <IconButton onClick={ (): void => applySearchParams({ prevCursor: questions?.meta.prevCursor }) }>
-              <ArrowLeftIcon className="w-4 h-4"/>
-            </IconButton>
-          ) }
-          { questions.meta.nextCursor && (
-            <IconButton onClick={ (): void => applySearchParams({ nextCursor: questions?.meta.nextCursor }) }>
-              <ArrowRightIcon className="w-4 h-4"/>
-            </IconButton>
-          ) }
-        </ButtonGroup>) }
-
-      { showClear() && <div>
-        <Button variant="outlined" onClick={ clearSearchParams }>Clear</Button>
-      </div> }
-    </div>
-
-    <table className="w-full table-auto text-left mt-4">
-      <thead>
-      <tr>
-        { tableColumns.map((head) => (
-          <th
-            key={ head }
-            className="bg-blue-gray-50/50 py-2 px-4">
-            <Typography
-              variant="small"
-              className="opacity-70">
-              { head }
-            </Typography>
-          </th>
-        )) }
-      </tr>
-      </thead>
-      <tbody>
-      { questions === undefined && <tr key={ 0 }>
-        <td colSpan={ tableColumns.length } className="p-5 text-center"><Spinner/></td>
-      </tr> }
-      { questions && questions.data.length === 0 && <tr key={ 0 }>
-        <td colSpan={ tableColumns.length } className="p-5 text-center">
-          <Typography variant="small">No data</Typography>
-        </td>
-      </tr> }
-      { questions && questions.data && questions.data.map((question: Question, index: number): ReactNode => (
-        <tr key={ index } className={ index === 0 ? 'border-b' : '' }>
-          <td className="py-2 px-4">
-            <Typography variant="small">
-              { index + 1 }
-            </Typography>
-          </td>
-
-          <td className="py-2 px-4">
-            <Tooltip content={ question.title }>
-              <Typography variant="small" className="capitalize truncate max-w-[400px]">
-                <Link
-                  key={ question.id }
-                  to={ Route.QUESTION.replace(':categoryId', question.categoryId!).replace(':questionId', question.id!) }>
-                  { question.title }
-                </Link>
+      <table className="w-full table-auto text-left mt-4">
+        <thead>
+        <tr>
+          { tableColumns.map((head) => (
+            <th
+              key={ head }
+              className="bg-blue-gray-50/50 py-2 px-4">
+              <Typography
+                variant="small"
+                className="opacity-70">
+                { head }
               </Typography>
-            </Tooltip>
-          </td>
-
-          <td className="py-2 px-4">
-            <Typography variant="small" className="capitalize">
-              { question.difficulty }
-            </Typography>
-          </td>
-
-          <td className="py-2 px-4">
-            <Typography variant="small" className="capitalize">
-              { question.type }
-            </Typography>
-          </td>
-
-          <td className="py-2 px-4">
-            <Rating readonly/>
-          </td>
-
-          <td className="py-2 px-4">
-            <div className="flex justify-end gap-1">
-              { auth && me === undefined ? <Spinner/> : checkAuth(Permission.UPDATE_QUESTION) &&
-                <AddQuestion question={ question } onSubmit={ refresh } iconButton/> }
-
-              { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_QUESTION) &&
-                <DeleteQuestion question={ question } onSubmit={ refresh } iconButton/> }
-            </div>
-          </td>
+            </th>
+          )) }
         </tr>
-      )) }
-      </tbody>
-    </table>
-  </>
+        </thead>
+        <tbody>
+        { questions === undefined && <tr key={ 0 }>
+          <td colSpan={ tableColumns.length } className="p-5 text-center"><Spinner/></td>
+        </tr> }
+        { questions && questions.data.length === 0 && <tr key={ 0 }>
+          <td colSpan={ tableColumns.length } className="p-5 text-center">
+            <Typography variant="small">No data</Typography>
+          </td>
+        </tr> }
+        { questions && questions.data && questions.data.map((question: Question, index: number): ReactNode => (
+          <tr key={ index } className={ index === 0 ? 'border-b' : '' }>
+            <td className="py-2 px-4">
+              <Typography variant="small">
+                { index + 1 }
+              </Typography>
+            </td>
+
+            <td className="py-2 px-4">
+              <Tooltip content={ question.title }>
+                <Typography variant="small" className="capitalize truncate max-w-[400px]">
+                  <Link
+                    key={ question.id }
+                    to={ Route.QUESTION.replace(':categoryId', question.categoryId!).replace(':questionId', question.id!) }>
+                    { question.title }
+                  </Link>
+                </Typography>
+              </Tooltip>
+            </td>
+
+            <td className="py-2 px-4">
+              <Typography variant="small" className="capitalize">
+                { question.difficulty }
+              </Typography>
+            </td>
+
+            <td className="py-2 px-4">
+              <Typography variant="small" className="capitalize">
+                { question.type }
+              </Typography>
+            </td>
+
+            <td className="py-2 px-4">
+              <Rating readonly/>
+            </td>
+
+            <td className="py-2 px-4">
+              <div className="flex justify-end gap-1">
+                { auth && me === undefined ? <Spinner/> : checkAuth(Permission.UPDATE_QUESTION) &&
+                  <AddQuestion question={ question } onSubmit={ refresh } iconButton/> }
+
+                { auth && me === undefined ? <Spinner/> : checkAuth(Permission.DELETE_QUESTION) &&
+                  <DeleteQuestion question={ question } onSubmit={ refresh } iconButton/> }
+              </div>
+            </td>
+          </tr>
+        )) }
+        </tbody>
+      </table>
+    </>
+  )
 }
