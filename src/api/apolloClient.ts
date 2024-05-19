5899334d73f81d0e54316d824ebee66064e6c39c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-19 16:25:41 -0600

    api query function added: process errors & ignore access denied

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import type { OperationVariables } from '@apollo/client/core/types'
+import type { QueryOptions } from '@apollo/client/core/watchQueryOptions'
 
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_API_BASE_URL,
@@ -23,7 +25,35 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-export default new ApolloClient({
+const apolloClient = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
+
+export default apolloClient
+
+export function apiQuery<T = any, TVariables extends OperationVariables = OperationVariables>(
+  options: QueryOptions<TVariables, T>,
+  setData: (data: T) => void,
+  setError: (message: string) => void,
+  setLoading: (loading: boolean) => void,
+): void {
+  setLoading(true)
+  apolloClient.query<T>(options)
+    .then(({ data, errors }): void => {
+      if (errors) {
+        const error = errors
+          .filter(error => !error.message.startsWith('Access denied!'))
+          .map(error => error.message).join('\n')
+
+        if (error) {
+          setError(error)
+          return
+        }
+      }
+
+      setData(data)
+    })
+    .catch((err): void => setError(err.message))
+    .finally((): void => setLoading(false))
+}
