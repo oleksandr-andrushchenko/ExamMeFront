9b1f8cdb1e6ca7b4ed2792d0df20cb0cbe759240
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-20 15:54:41 -0600

    all fields became optional

--- src/schema/exam/ExamQuestion.ts
+++ src/schema/exam/ExamQuestion.ts
@@ -1,9 +1,9 @@
 export default interface ExamQuestion {
-  number: number
-  question: string
-  type: string
-  difficulty: string
-  choices: string[]
-  choice: number
-  answer: string
+  number?: number
+  question?: string
+  type?: string
+  difficulty?: string
+  choices?: string[]
+  choice?: number
+  answer?: string
 }
