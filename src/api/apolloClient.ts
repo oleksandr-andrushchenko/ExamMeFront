bb2f40adbf9fd7674002827829068f8c2e619d00
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-21 22:38:05 -0600

    api mutate function added; typenames removed

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-import type { OperationVariables } from '@apollo/client/core/types'
-import type { QueryOptions } from '@apollo/client/core/watchQueryOptions'
+import type { DefaultContext, OperationVariables } from '@apollo/client/core/types'
+import type { MutationOptions, QueryOptions } from '@apollo/client/core/watchQueryOptions'
+import type { ApolloCache } from '@apollo/client/cache'
 
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_API_BASE_URL,
@@ -27,7 +28,9 @@ const authLink = setContext((_, { headers }) => {
 
 const apolloClient = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 })
 
 export default apolloClient
@@ -40,6 +43,37 @@ export function apiQuery<T = any, TVariables extends OperationVariables = Operat
 ): void {
   setLoading(true)
   apolloClient.query<T>(options)
+    .then(({ data, errors }): void => {
+      if (errors) {
+        const error = errors
+          .filter(error => !error.message.startsWith('Access denied!'))
+          .map(error => error.message).join('\n')
+
+        if (error) {
+          setError(error)
+          return
+        }
+      }
+
+      setData(data)
+    })
+    .catch((err): void => setError(err.message))
+    .finally((): void => setLoading(false))
+}
+
+export function apiMutate<
+  TData = any,
+  TVariables extends OperationVariables = OperationVariables,
+  TContext extends Record<string, any> = DefaultContext,
+  TCache extends ApolloCache<any> = ApolloCache<any>
+>(
+  options: MutationOptions<TData, TVariables, TContext>,
+  setData: (data: TData) => void,
+  setError: (message: string) => void,
+  setLoading: (loading: boolean) => void,
+): void {
+  setLoading(true)
+  apolloClient.mutate<TData, TVariables, TContext, TCache>(options)
     .then(({ data, errors }): void => {
       if (errors) {
         const error = errors
