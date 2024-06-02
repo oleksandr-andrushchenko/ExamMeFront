2cb9a86ab0da813edb89349fdb5a78e2cd2a9a37
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-06-02 11:52:50 -0600

    button & text types support added

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,4 +1,40 @@
-import { Spinner as TailwindSpinner } from '@material-tailwind/react'
-import { ReactNode } from 'react'
+import { Button, Spinner as TailwindSpinner, Typography } from '@material-tailwind/react'
+import React, { ReactNode } from 'react'
 
-export default (): ReactNode => <TailwindSpinner className="h-8 w-8 text-gray-900/50"/>
+interface Props {
+  type?: 'button' | 'text'
+  height?: number
+  width?: number
+  children?: React.ReactNode
+}
+
+export default function Spinner({ type, height, width, children }: Props): ReactNode {
+  if (type === 'button') {
+    return (
+      <span className="animate-pulse">
+        <Button
+          disabled
+          tabIndex={ -1 }
+          className={ `${ height ?? 'h-8' } ${ width ?? 'w-24' } bg-gray-300 shadow-none hover:shadow-none` }
+        >
+        { children }
+      </Button>
+      </span>
+    )
+  }
+
+  if (type === 'text') {
+    return (
+      <Typography
+        as="span"
+        className={ `animate-pulse inline-block ${ height ?? 'h-2' } ${ width ?? 'w-24' } rounded-full bg-gray-300` }
+      >
+        { children }
+      </Typography>
+    )
+  }
+
+  return (
+    <TailwindSpinner className="h-8 w-8 text-gray-900/50"/>
+  )
+}
