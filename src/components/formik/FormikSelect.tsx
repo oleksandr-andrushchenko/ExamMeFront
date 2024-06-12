4a91c431c79efbec2caf82232ab3a7737a6165a0
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-06-12 02:54:36 -0600

    formik select added

---
+++ src/components/formik/FormikSelect.tsx
@@ -0,0 +1,37 @@
+import { useField } from 'formik'
+import Error from '../Error'
+import React from 'react'
+import { Option, Select } from '@material-tailwind/react'
+
+export default function FormikSelect({ name, label, options }) {
+  const [ input, meta, helper ] = useField({ name })
+
+  return (
+    <>
+      <Select
+        name={ name }
+        label={ label || name }
+        onChange={ value => helper.setValue(value) }
+        onBlur={ input.onBlur }
+        // containerProps={ {
+        //   onBlur: (e) => !e.nativeEvent.relatedTarget && helper.setTouched(false),
+        // } }
+        defaultValue={ meta.value }
+        // value={ meta.value }
+        success={ meta.touched && !meta.error }
+        error={ meta.touched && !!meta.error }
+        className="capitalize">
+        { options.map(option => (
+          <Option
+            key={ option.key || option.value }
+            value={ option.value }
+            className="capitalize">
+            { option.label }
+          </Option>
+        )) }
+      </Select>
+
+      { meta.touched && meta.error && <Error text={ meta.error }/> }
+    </>
+  )
+};
