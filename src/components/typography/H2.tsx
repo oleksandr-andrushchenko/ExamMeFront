bc7e8842f492371815b5ab5f7b37bfcfc71b461f
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-07-14 16:21:34 -0600

    h2 component element added

---
+++ src/components/typography/H2.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@material-tailwind/react'
+import React, { memo } from 'react'
+
+const H1 = ({ icon, label, children }) => {
+  return <Typography as="h2" variant="h6" className="mt-4">
+    { icon && React.createElement(icon, { className: 'h-4 w-4 inline-block' }) }
+    { icon && ' ' }
+    { label || children }
+  </Typography>
+}
+
+export default memo(H1)
