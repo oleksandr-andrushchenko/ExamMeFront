ac1c2cd0b2517b6c243b985cfc8bb5e968ef2bbc
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-08-04 17:31:55 -0600

    action buttons added to activities

--- src/components/activity/CategoryApprovedActivity.tsx
+++ src/components/activity/CategoryApprovedActivity.tsx
@@ -2,20 +2,26 @@ import { ComponentProps, memo } from 'react'
 import Activity from '../../schema/activity/Activity'
 import Route from '../../enum/Route'
 import Link from '../elements/Link'
+import AddExam from '../exam/AddExam'
 
 interface Props extends ComponentProps<any> {
   activity: Activity
 }
 
 const CategoryApprovedActivity = ({ activity }: Props) => {
-  const link = Route.Category.replace(':categoryId', activity.categoryId!)
+  const category = { id: activity.categoryId! }
+  const link = Route.Category.replace(':categoryId', category.id)
 
   return <>
     <b><Link label={ activity.categoryName } to={ link }/></b>
     { ' ' }
     category has been approved. You can start your exam
     { ' ' }
-    <b><Link label="here" to={ link }/></b>.
+    <b><Link label="here" to={ link }/></b>
+    { ' ' }
+    or
+    { ' ' }
+    <AddExam category={ category }/>
   </>
 }
 
--- src/components/activity/CategoryCreatedActivity.tsx
+++ src/components/activity/CategoryCreatedActivity.tsx
@@ -2,20 +2,26 @@ import { ComponentProps, memo } from 'react'
 import Activity from '../../schema/activity/Activity'
 import Route from '../../enum/Route'
 import Link from '../elements/Link'
+import AddQuestion from '../question/AddQuestion'
 
 interface Props extends ComponentProps<any> {
   activity: Activity
 }
 
 const CategoryCreatedActivity = ({ activity }: Props) => {
-  const link = Route.Category.replace(':categoryId', activity.categoryId!)
+  const category = { id: activity.categoryId! }
+  const link = Route.Category.replace(':categoryId', category.id)
 
   return <>
     <b><Link label={ activity.categoryName } to={ link }/></b>
     { ' ' }
     category has been created. You can add your own questions
     { ' ' }
-    <b><Link label="here" to={ link }/></b>.
+    <b><Link label="here" to={ link }/></b>
+    { ' ' }
+    or
+    { ' ' }
+    <AddQuestion category={ category }/>
   </>
 }
 
